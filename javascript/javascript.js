45ec615492eba58884db8caf2d961b86e8e06e23
roccomoresi <137230323+roccomoresi@users.noreply.github.com>
2023-11-16 12:58:26 -0300

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,8 @@
+// donate.js
+
+document.getElementById('donateButton').addEventListener('click', function () {
+    // Redirige a la página de donación
+    window.location.href = './donacion.html';
+});
+
+
